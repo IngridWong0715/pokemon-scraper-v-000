1ce483135468215f9676b965d12984720979811f
Ingrid Wong <yw340@georgetown.edu>
2018-02-09 12:53:31 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -15,7 +15,7 @@ class Pokemon
   def self.find(id, db)
     stm = db.prepare("SELECT * FROM pokemon WHERE id = ?")
     stm.bind_param id, id
-    rs = stm.execute.flatten
+    row = stm.execute.flatten
     row = rs.next
     self.new(id: id, db: db, name: row[1], type: row[2])
   end
